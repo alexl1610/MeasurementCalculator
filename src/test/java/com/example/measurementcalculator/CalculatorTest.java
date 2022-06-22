7785b77f63dbef10a9cf6bf464447905c9af0d98
Alexander Lang <alexander.lang1610@gmail.com>
2022-06-22 15:45:55 +0200

    added Test Class and ounce Test

---
+++ src/test/java/com/example/measurementcalculator/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.example.measurementcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    private Calculator calc = new Calculator();
+
+    @Test
+    void ounceTest() {
+        int a = 1;
+
+        double expected = 28.35;
+
+        double result = calc.ounce(a);
+
+        assertEquals(expected, result);
+    }
+
+}
